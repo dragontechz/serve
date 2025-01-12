f51d212b5390cad93d9f6d4bd7968e5b1f3708c2
dragontechz <juleskono58@gmail.com>
2025-01-12 12:02:31 +0100

    Add files via upload

---
+++ scantraffic.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+var buff []byte = make([]byte, 1024)
+func main(){
+		sock, err := net.Listen("tcp", ":7777")
+		if err != nil {
+			fmt.Println("ERROR: ", err)
+		}
+	
+		fmt.Println("server listening on port 7777")
+	
+		for {
+			conn, err := sock.Accept()
+			if err != nil {
+				log.Println("ERROR: ", err)
+			}
+			log.Println("new connection established by:", conn.RemoteAddr().String())
+			
+			go display_traffic((conn))
+			continue
+	
+		}
+
+}
+func display_traffic(conn net.Conn){
+	for {
+		n , err := conn.Read(buff)
+		if err != nil {
+			log.Println("ERROR:",err)
+		}
+
+		data := string(buff[:n])
+
+		log.Println("data: ",data)
+
+	}
+}
