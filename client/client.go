e8769d6c107dba08b00fd01b7f4bd3a129de5528
dragontechz <juleskono58@gmail.com>
2025-01-12 08:17:27 +0100

    Add files via upload

---
+++ client/client.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"log"
+
+	"github.com/quic-go/quic-go"
+)
+
+func main() {
+	addr := "localhost:4242"
+	ctx := context.Background()
+	config := &tls.Config{
+		InsecureSkipVerify: true,                       // À ne pas utiliser en production
+		NextProtos:         []string{"h3", "http/1.1"}, // Spécifie les protocoles souhaités
+	}
+	quicConfig := &quic.Config{} // Tu peux personnaliser cette configuration si nécessaire
+
+	session, err := quic.DialAddr(ctx, addr, config, quicConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	stream, err := session.OpenStream()
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		_, err = stream.Write([]byte("salut"))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		buf := make([]byte, 1024)
+		n, err := stream.Read(buf)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println("Réponse du serveur:", string(buf[:n]))
+		continue
+	}
+}
+
+func generateTLSConfig() *tls.Config {
+	return &tls.Config{
+		InsecureSkipVerify: true,
+		NextProtos:         []string{"quic-echo-example"},
+	}
+}
